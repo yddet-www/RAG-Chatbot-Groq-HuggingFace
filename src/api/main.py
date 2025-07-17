82e8249ccb29e4002da66a16b326c9e3cebbe29c
yddet-www <yddet.www@gmail.com>
2025-07-16 19:00:58 -0500

    Add missing middleware

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI, HTTPException, Query, UploadFile, File, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import os
 import shutil
@@ -10,6 +11,13 @@ from typing import List
 from fastapi import UploadFile, File
 
 app = FastAPI(title="RAGStack AI (LangGraph Edition)")
+
+app.add_middleware(CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
+
 rag_pipeline = RAGPipeline()
 
 class Question(BaseModel):
